a2baa9bc71df9d1ff9c1c5771c6f7f90617e9e04
Kynpie <dzakiy0404@gmail.com>
2025-04-22 09:17:08 +0700

    model belongsto ama hasMany

--- FoodOrderingSystem/app/Models/Transaction.php
+++ FoodOrderingSystem/app/Models/Transaction.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Transaction extends Model
 {
@@ -11,4 +12,11 @@ class Transaction extends Model
     protected $table = 'transactions';
     protected $primaryKey = 'id';
     public $timestamps = true;
+
+    public function payments():BelongsTo{
+        return $this->belongsTo(Payments::class,'payments_id');
+    }
+    public function user():BelongsTo{
+        return $this->belongsTo(User::class,'users_id');
+    }
 }
