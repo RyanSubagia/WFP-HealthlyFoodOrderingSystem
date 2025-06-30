92437b593544bba85bed55466340cbcc32f459b2
yan <150877487+RyanSubagia@users.noreply.github.com>
2025-06-30 12:30:11 +0700

    Seeder Transaction

--- FoodOrderingSystem/database/seeders/DatabaseSeeder.php
+++ FoodOrderingSystem/database/seeders/DatabaseSeeder.php
@@ -21,6 +21,8 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             PaymentSeeder::class,
             TransactionSeeder::class,
+            TransactionItemSeeder::class,
+
         ]);
     }
 }
